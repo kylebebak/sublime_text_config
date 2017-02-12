54efe72bc3c9c6510669a2ae679572ccaeb6cedc
Kyle Bebak <kylebebak@gmail.com>
2017-02-12 12:40:24 -0600

    Fixed bug which appeared in CycleColorSchemeCommand, due to the fact that ST can unpredictably change the directory from which the current color scheme is loaded without reloading settings. It doesn't change the color scheme, it just suddenly loads the same scheme from a different directory

--- cycle_color_scheme.py
+++ cycle_color_scheme.py
@@ -4,13 +4,14 @@ class CycleColorSchemeCommand(sublime_plugin.TextCommand):
     def run(self, edit, **kwargs):
 
         preferences = sublime.load_settings('Preferences.sublime-settings')
-        scheme = self.view.settings().get("color_scheme")
+        scheme = self.view.settings().get("color_scheme").split('/')[-1]
 
         try:
+            scheme_directory = kwargs.get("scheme_directory")
             schemes = kwargs.get("color_schemes")
             i = schemes.index(scheme)
-            preferences.set(
-                'color_scheme', schemes[ (i+1) % len(schemes) ])
+            new_scheme = schemes[ (i+1) % len(schemes) ]
+            preferences.set('color_scheme', '{}/{}'.format(scheme_directory, new_scheme))
         except ValueError:
             print("Your current color scheme doesn't match any of your args.")
         except Exception:
