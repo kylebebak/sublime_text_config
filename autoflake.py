5a44e67977fb22b8011d14176c58ca9e9be4d75e
Kyle Bebak <kylebebak@gmail.com>
2017-03-31 20:41:29 -0600

    Created a very simple plugin that invokes autoflake to remove unused imports from the file in the current view

---
+++ autoflake.py
@@ -0,0 +1,13 @@
+import sublime, sublime_plugin
+import subprocess
+from os import getenv
+
+
+class Autoflake(sublime_plugin.TextCommand):
+
+    autoflake_path = '{}/{}'.format(
+        getenv('HOME'), '/.local/bin/autoflake'
+    )
+
+    def run(self, edit, **kwargs):
+        subprocess.check_call([ self.autoflake_path, '--in-place', '--remove-all-unused-imports', self.view.file_name() ])
