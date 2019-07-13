4c49999139b7576776a06eed812db22416322d89
Kyle Bebak <kylebebak@gmail.com>
2019-07-13 10:11:06 -0500

    Various updates

--- file_path_and_line_number.py
+++ file_path_and_line_number.py
@@ -1,12 +1,18 @@
 try:
-    from typing import Any, Optional, List, Tuple
+    from typing import Any, Optional, List, NamedTuple, cast
 except Exception:
-    pass
+    NamedTuple = lambda name, values: ''  # type: ignore # noqa
+    cast = lambda t, val: val  # noqa
 
 import sublime  # type: ignore
 import sublime_plugin  # type: ignore
 
+from collections import namedtuple
 import os
+
+
+BookmarkType = NamedTuple('Point', [('row', int), ('col', int), ('text', str)])
+Bookmark = namedtuple('Bookmark', ['row', 'col', 'text'])
 
 
 def truncate(s: str, n: int) -> str:
@@ -15,9 +21,16 @@ def truncate(s: str, n: int) -> str:
     return s
 
 
+def format_bookmark(path, b, include_text):
+    # type: (str, BookmarkType, bool) -> str
+    if include_text:
+        return '{}:{}:{}    {}'.format(path, str(b.row + 1), str(b.col + 1), b.text)
+    return '{}:{}:{}'.format(path, str(b.row + 1), str(b.col + 1))
+
+
 class CopyFilePathAndLineNumberCommand(sublime_plugin.TextCommand):
 
-    def run(self, edit, strip: bool = True, max_len: int = 50) -> None:
+    def run(self, edit, strip: bool = True, max_len: int = 50, include_text: bool = False) -> None:
         path = self.view.file_name()  # type: Optional[str]
         if not path:
             return
@@ -25,10 +38,10 @@ class CopyFilePathAndLineNumberCommand(sublime_plugin.TextCommand):
         if project_path:
             path = os.path.relpath(path, project_path)
 
-        lines = []  # type: List[Tuple[int, int, str]]
+        bookmarks = []  # type: List[BookmarkType]
         for r in self.view.sel():
-            lines.append(self.get_line_number_and_text(r.begin(), strip, max_len))
-        text = '\n'.join('{}:{}:{}    {}'.format(path, str(line[0] + 1), str(line[1] + 1), line[2]) for line in lines)
+            bookmarks.append(self.get_line_number_and_text(r.begin(), strip, max_len))
+        text = '\n'.join(format_bookmark(path, b, include_text) for b in bookmarks)
         sublime.set_clipboard(text)
         self.view.window().status_message('copied - {}'.format(truncate(text, 80)))
 
@@ -43,9 +56,9 @@ class CopyFilePathAndLineNumberCommand(sublime_plugin.TextCommand):
             return None
 
     def get_line_number_and_text(self, point, strip, max_len):
-        # type: (Any, int, bool, int) -> Tuple[int, int, str]
+        # type: (Any, int, bool, int) -> BookmarkType
         row, col = self.view.rowcol(point)
         line = self.view.substr(self.view.line(point))  # type: str
         if strip:
             line = line.strip()
-        return row, col, truncate(line, max_len)
+        return cast(BookmarkType, Bookmark(row, col, truncate(line, max_len)))
