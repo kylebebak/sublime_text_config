4a8c686f1cd2fedea398a4649bd26d271d08884a
Kyle Bebak <kyle@elementaryml.com>
2023-10-22 23:08:42 -0600

    Add tree_sitter_choose_siblings, tree_sitter_choose_descendants commands

--- tree_sitter_commands.py
+++ tree_sitter_commands.py
@@ -1,7 +1,7 @@
 import os
 
 import sublime_plugin
-from sublime_tree_sitter import get_larger_region, get_tree_dict, walk_tree
+from sublime_tree_sitter import get_larger_region, get_node_spanning_region, get_tree_dict, query_tree, walk_tree
 
 
 class TreeSitterPrintTreeCommand(sublime_plugin.TextCommand):
@@ -21,8 +21,51 @@ class TreeSitterPrintTreeCommand(sublime_plugin.TextCommand):
 
 
 class TreeSitterSelectLargerNodeCommand(sublime_plugin.TextCommand):
+    """
+    Note: we scroll to start of larger node if it's not visible.
+    """
     def run(self, edit):
         for region in self.view.sel():
             new_region = get_larger_region(region, self.view)
             if new_region:
                 self.view.sel().add(new_region)
+
+                new_begin = new_region.begin()
+                visible_region = self.view.visible_region()
+                if new_region.begin() not in visible_region:
+                    self.view.show(new_begin)
+
+
+class TreeSitterChooseSiblingsCommand(sublime_plugin.TextCommand):
+    """
+    TODO: selecting all text
+    """
+    def run(self, edit):
+        node = get_node_spanning_region(self.view.sel()[0], self.view.buffer_id())
+        print(node)
+        if not node or not node.parent:
+            return
+        siblings = node.parent.children
+        for node in siblings:
+            print(node)
+
+
+class TreeSitterChooseDescendantsCommand(sublime_plugin.TextCommand):
+    """
+    TODO:
+
+    - Selecting all text should let us get root node, might require change to `get_node_spanning_region`
+    - Specify query_name to look up query and use it
+    """
+    def run(self, edit, query_name: str):
+        buffer_id = self.view.buffer_id()
+        tree_dict = get_tree_dict(buffer_id)
+        if not tree_dict:
+            return
+
+        node = get_node_spanning_region(self.view.sel()[0], buffer_id)
+        if not node:
+            return
+        nodes = query_tree(tree_dict["scope"], "", node) or []
+        for node in nodes:
+            print(node[0])
