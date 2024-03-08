de0ee6afc09745974ad239f859573268d501fa89
Kyle Bebak <kyle@elementaryml.com>
2024-03-08 17:04:02 -0600

    - Add commands to copy relative and absolute file path - These used to ship with https://github.com/titoBouzout/SideBarEnhancements - Removed from package in commit d6f12d9c37063ca8a86c34e7068627893db76f30

--- file_and_view_commands.py
+++ file_and_view_commands.py
@@ -10,12 +10,10 @@ import sublime_plugin
 from .utils import maybe_none, not_none
 
 
-class DeleteOpenFileCommand(sublime_plugin.TextCommand):
-    def run(self, *args, **kwargs) -> None:
-        path: str | None = self.view.file_name()
-        if not path:
-            return
-        os.remove(path)
+def truncate(s: str, n: int) -> str:
+    if len(s) > n:
+        return "{}…".format(s[:n])
+    return s
 
 
 class CopyViewNameCommand(sublime_plugin.TextCommand):
@@ -28,14 +26,18 @@ class CopyViewNameCommand(sublime_plugin.TextCommand):
         not_none(self.view.window()).status_message(f'copied "{name}"')
 
 
+class CopyFilePathCommand(sublime_plugin.TextCommand):
+    def run(self, edit, relative: bool = True) -> None:
+        if not (path := self.view.file_name()):
+            return
+        if (project_path := get_project_path(self.view)) and relative:
+            path = os.path.relpath(path, project_path)
+        sublime.set_clipboard(path)
+        not_none(self.view.window()).status_message("copied - {}".format(truncate(path, 80)))
+
+
 BookmarkType = NamedTuple("Point", [("row", int), ("col", int), ("text", str)])
 Bookmark = namedtuple("Bookmark", ["row", "col", "text"])
-
-
-def truncate(s: str, n: int) -> str:
-    if len(s) > n:
-        return "{}…".format(s[:n])
-    return s
 
 
 def format_bookmark(path: str, b: BookmarkType, include_text: bool) -> str:
@@ -44,13 +46,21 @@ def format_bookmark(path: str, b: BookmarkType, include_text: bool) -> str:
     return "{}:{}:{}".format(path, str(b.row + 1), str(b.col + 1))
 
 
+def get_project_path(view: sublime.View) -> str | None:
+    project = not_none(view.window()).project_data()
+    if maybe_none(project) is None:
+        return None
+    try:
+        return os.path.expanduser(cast(dict, project)["folders"][0]["path"])
+    except Exception:
+        return None
+
+
 class CopyFilePathAndLineNumberCommand(sublime_plugin.TextCommand):
     def run(self, edit, strip: bool = True, max_len: int = 50, include_text: bool = False) -> None:
-        path: str | None = self.view.file_name()
-        if not path:
+        if not (path := self.view.file_name()):
             return
-        project_path = self.project_path()
-        if project_path:
+        if project_path := get_project_path(self.view):
             path = os.path.relpath(path, project_path)
 
         bookmarks: list[BookmarkType] = []
@@ -59,15 +69,6 @@ class CopyFilePathAndLineNumberCommand(sublime_plugin.TextCommand):
         text = "\n".join(format_bookmark(path, b, include_text) for b in bookmarks)
         sublime.set_clipboard(text)
         not_none(self.view.window()).status_message("copied - {}".format(truncate(text, 80)))
-
-    def project_path(self) -> str | None:
-        project = not_none(self.view.window()).project_data()
-        if maybe_none(project) is None:
-            return None
-        try:
-            return os.path.expanduser(cast(dict, project)["folders"][0]["path"])
-        except Exception:
-            return None
 
     def get_line_number_and_text(self, point: int, strip: bool, max_len: int) -> BookmarkType:
         row, col = self.view.rowcol(point)
